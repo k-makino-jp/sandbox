730604f50de97efbaba8fea63eb7e7ac8022b661
MfsTeller <i1211435.kosen@gmail.com>
2021-02-21 00:06:04 +0900

    modify

--- goaes/pkg/azure/azure.go
+++ goaes/pkg/azure/azure.go
@@ -14,10 +14,25 @@ import (
 
 var (
 	queueEndpoint = "https://myaccount.queue.core.windows.net/images-to-download"
+	// Create a request pipeline object configured with credentials and with pipeline options. Once created,
+	// a pipeline object is goroutine-safe and can be safely used with many XxxURL objects simultaneously.
+	pipeline = azqueue.NewPipeline(
+		azqueue.NewAnonymousCredential(), // A pipeline always requires some credential object
+		azqueue.PipelineOptions{
+			Retry: azqueue.RetryOptions{
+				Policy:        azqueue.RetryPolicyExponential, // Use exponential backoff as opposed to fixed
+				MaxTries:      3,                              // Try at most 3 times to perform the operation (set to 1 to disable retries)
+				TryTimeout:    time.Second * 3,                // Maximum time allowed for any single try
+				RetryDelay:    time.Second * 1,                // Backoff amount for each retry (exponential or fixed)
+				MaxRetryDelay: time.Second * 3,                // Max delay between retries
+			},
+		},
+	)
 )
 
 // Azure Queue Storage処理向けインターフェース
 type Azure interface {
+	InitPipeline()
 	Enqueue()
 }
 
@@ -42,13 +57,21 @@ type azure struct {
 	enqueueMessageResponse enqueueMessageResponse
 }
 
+func (a *azure) InitPipeline(sas Sas) {
+	// Configure sas
+	URL, _ := url.Parse(queueEndpoint)
+	query := url.Values{}
+	query.Set("sv", sas.Sv)
+	URL.RawQuery = query.Encode()
+	// Create a messagesURL object
+	// XxxURL object contains a URL and a request pipeline.
+	a.messagesURLEnqueue = azqueue.NewMessagesURL(*URL, pipeline)
+}
+
 // Enqueue Azure Queue Storage REST API "Put messages"
 func (a azure) Enqueue(message Message) (int, error) {
 	// convert struct to json text
-	jsonBytes, err := json.Marshal(message)
-	if err != nil {
-		return 0, err
-	}
+	jsonBytes, _ := json.Marshal(message)
 	messageText := string(jsonBytes)
 
 	// Put messages
@@ -61,46 +84,18 @@ func (a azure) Enqueue(message Message) (int, error) {
 		visibilityTimeout, // 可視性タイムアウト
 		timeToLive,        // メッセージの有効期限
 	)
+	statusCode := a.enqueueMessageResponse.StatusCode(enqueueMessageResponse)
 	if err != nil {
-		return 0, err
+		return statusCode, err
 	}
 	// status code check
-	statusCode := a.enqueueMessageResponse.StatusCode(enqueueMessageResponse)
 	if statusCode == http.StatusCreated {
 		return statusCode, nil
 	}
 	return statusCode, errors.New("Server connection error occurred")
 }
 
-var (
-	// Create a request pipeline object configured with credentials and with pipeline options. Once created,
-	// a pipeline object is goroutine-safe and can be safely used with many XxxURL objects simultaneously.
-	pipeline = azqueue.NewPipeline(
-		azqueue.NewAnonymousCredential(), // A pipeline always requires some credential object
-		azqueue.PipelineOptions{
-			Retry: azqueue.RetryOptions{
-				Policy:        azqueue.RetryPolicyExponential, // Use exponential backoff as opposed to fixed
-				MaxTries:      3,                              // Try at most 3 times to perform the operation (set to 1 to disable retries)
-				TryTimeout:    time.Second * 3,                // Maximum time allowed for any single try
-				RetryDelay:    time.Second * 1,                // Backoff amount for each retry (exponential or fixed)
-				MaxRetryDelay: time.Second * 3,                // Max delay between retries
-			},
-		},
-	)
-)
-
 // NewAzure コンストラクタ
-func NewAzure(sas Sas) *azure {
-	// Configure sas
-	URL, _ := url.Parse(queueEndpoint)
-	query := url.Values{}
-	query.Set("sv", sas.Sv)
-	URL.RawQuery = query.Encode()
-
-	// Create a messagesURL object
-	// XxxURL object contains a URL and a request pipeline.
-	messagesURL := azqueue.NewMessagesURL(*URL, pipeline)
-	return &azure{
-		messagesURLEnqueue: messagesURL,
-	}
+func NewAzure() *azure {
+	return &azure{}
 }
--- goaes/pkg/azure/azure_test.go
+++ goaes/pkg/azure/azure_test.go
@@ -3,6 +3,7 @@ package azure
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"gosample/pkg/azure/mock_azure"
 	"net/url"
 	"reflect"
@@ -13,11 +14,46 @@ import (
 	"github.com/golang/mock/gomock"
 )
 
+func Test_azure_InitPipeline(t *testing.T) {
+	sampleSas := Sas{}
+	URL, _ := url.Parse(queueEndpoint)
+	query := url.Values{}
+	query.Set("sv", sampleSas.Sv)
+	URL.RawQuery = query.Encode()
+	type args struct {
+		sas Sas
+	}
+	tests := []struct {
+		name      string
+		a         *azure
+		args      args
+		wantAzure *azure
+	}{
+		{
+			name: "azure.InitPipeline Pipelineを初期化するとき Pipelineが初期化されること",
+			a:    &azure{},
+			args: args{sas: sampleSas},
+			wantAzure: &azure{
+				messagesURLEnqueue:     azqueue.NewMessagesURL(*URL, pipeline),
+				enqueueMessageResponse: nil,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.a.InitPipeline(tt.args.sas)
+			if !reflect.DeepEqual(tt.a, tt.wantAzure) {
+				t.Errorf("InitPipeline() got = %v, wantAzure %v", tt.a, tt.wantAzure)
+			}
+		})
+	}
+}
+
 func Test_azure_Enqueue(t *testing.T) {
 	// create controller
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
-	// create mock
+	// // create mock
 	mockMessagesURLEnqueue := mock_azure.NewMockmessagesURLEnqueue(ctrl)
 	mockEnqueueMessageResponse := mock_azure.NewMockenqueueMessageResponse(ctrl)
 	type args struct {
@@ -61,6 +97,64 @@ func Test_azure_Enqueue(t *testing.T) {
 			},
 			testTeardown: func() {},
 		},
+		{
+			name: "azure.Enqueue リクエストに失敗したとき Errorが返ること",
+			a: azure{
+				messagesURLEnqueue:     mockMessagesURLEnqueue,
+				enqueueMessageResponse: mockEnqueueMessageResponse,
+			},
+			args:    args{message: Message{}},
+			want:    0,
+			wantErr: true,
+			testSetup: func() {
+				jsonBytes, _ := json.Marshal(Message{})
+				messageText := string(jsonBytes)
+				ctx := context.TODO()
+				visibilityTimeout := time.Second * 0
+				timeToLive := time.Minute
+				sampleEnqueueMessageResponse := azqueue.EnqueueMessageResponse{}
+				mockMessagesURLEnqueue.
+					EXPECT().
+					Enqueue(ctx, messageText, visibilityTimeout, timeToLive).
+					Return(&sampleEnqueueMessageResponse, errors.New("Azure Queue Storage Error Occurred")).
+					Times(1)
+				mockEnqueueMessageResponse.
+					EXPECT().
+					StatusCode(&sampleEnqueueMessageResponse).
+					Return(0).
+					Times(1)
+			},
+			testTeardown: func() {},
+		},
+		{
+			name: "azure.Enqueue StatusCode201以外が返るとき Errorが返ること",
+			a: azure{
+				messagesURLEnqueue:     mockMessagesURLEnqueue,
+				enqueueMessageResponse: mockEnqueueMessageResponse,
+			},
+			args:    args{message: Message{}},
+			want:    500,
+			wantErr: true,
+			testSetup: func() {
+				jsonBytes, _ := json.Marshal(Message{})
+				messageText := string(jsonBytes)
+				ctx := context.TODO()
+				visibilityTimeout := time.Second * 0
+				timeToLive := time.Minute
+				sampleEnqueueMessageResponse := azqueue.EnqueueMessageResponse{}
+				mockMessagesURLEnqueue.
+					EXPECT().
+					Enqueue(ctx, messageText, visibilityTimeout, timeToLive).
+					Return(&sampleEnqueueMessageResponse, nil).
+					Times(1)
+				mockEnqueueMessageResponse.
+					EXPECT().
+					StatusCode(&sampleEnqueueMessageResponse).
+					Return(500).
+					Times(1)
+			},
+			testTeardown: func() {},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -79,38 +173,18 @@ func Test_azure_Enqueue(t *testing.T) {
 }
 
 func TestNewAzure(t *testing.T) {
-	// Configure sas
-	URL, _ := url.Parse(queueEndpoint)
-	query := url.Values{}
-	query.Set("sv", "sv")
-	URL.RawQuery = query.Encode()
-
-	// Create a messagesURL object
-	// XxxURL object contains a URL and a request pipeline.
-	sampleMessagesURL := azqueue.NewMessagesURL(*URL, pipeline)
-	type args struct {
-		sas Sas
-	}
 	tests := []struct {
 		name string
-		args args
 		want *azure
 	}{
 		{
 			name: "NewAzure インスタンスを生成するとき インスタンスが返ること",
-			args: args{
-				sas: Sas{
-					Sv: "sv",
-				},
-			},
-			want: &azure{
-				messagesURLEnqueue: sampleMessagesURL,
-			},
+			want: &azure{},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := NewAzure(tt.args.sas); !reflect.DeepEqual(got, tt.want) {
+			if got := NewAzure(); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("NewAzure() = %v, want %v", got, tt.want)
 			}
 		})
