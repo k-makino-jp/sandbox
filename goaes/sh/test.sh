49d398e7a82a6fb41ff5841cc597874e5a55e286
MfsTeller <i1211435.kosen@gmail.com>
2021-03-05 06:35:39 +0900

    modify

---
+++ goaes/sh/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Hello world"
+
+# z: filter the archive through gzip
+# c: create a new archive
+# f: target archive file
+# v: verbosely list files processed
+# tar -zcvf xxxx.tar.gz directory
