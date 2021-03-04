da3dd6ba30dd50520c362ac416b67912cc08f13f
MfsTeller <i1211435.kosen@gmail.com>
2021-03-05 07:15:54 +0900

    modify

---
+++ goaes/go.mod
@@ -0,0 +1,9 @@
+module gosample
+
+go 1.16
+
+// require k8s.io/client-go v0.20.3
+
+// require k8s.io/client-go v0.20.4
+
+require k8s.io/client-go v0.20.4
--- goaes/main.go
+++ goaes/main.go
@@ -1,6 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"k8s.io/client-go/rest"
+	// go install k8s.io/client-go/rest@v0.20.3
+	// go install golang.org/x/tools/gopls@latest
+)
+
+// update go.mod
+//   go mod tidy: add missing and remove unused modules
+//   go build command uses require version
+// download module
+//   go install k8s.io/client-go/rest@v0.20.3
+// go build
+
+// Kubernetes Kubernetes REST API処理用インターフェース
+type clientGoRest interface {
+	InClusterConfig() (*rest.Config, error)
+}
+
+type clientGoRestImpl struct{}
+
+func (r clientGoRestImpl) InClusterConfig() (*rest.Config, error) {
+	return rest.InClusterConfig()
+}
 
 func main() {
 	fmt.Println("Hello world")
