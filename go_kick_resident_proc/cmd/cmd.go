db84bb74dd1e3176cc7d6f1158c66ab158e407a4
MfsTeller <i1211435.kosen@gmail.com>
2021-12-11 16:01:05 +0900

    test

--- go_kick_resident_proc/cmd/cmd.go
+++ go_kick_resident_proc/cmd/cmd.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"syscall"
 	"time"
 
 	"github.com/shirou/gopsutil/process"
@@ -66,5 +67,37 @@ func ExecOsCommandWithTimeout(cmd string, envKeyValueMap map[string]string, time
 			return execCmd.ProcessState.ExitCode(), stdoutStderrBuffer.String(), err
 		}
 	}
+	return execCmd.ProcessState.ExitCode(), stdoutStderrBuffer.String(), err
+}
+
+// ExecOsCommandWithTimeout executes os command considering timeout.
+func ExecOsCommandWithTimeoutLinux(cmd string, envKeyValueMap map[string]string, timeout time.Duration) (exitCode int, stdoutStderr string, err error) {
+	execCmd := exec.Command("sh", "-c", cmd)
+	execCmd.Env = os.Environ()
+	for key, value := range envKeyValueMap {
+		keyValue := fmt.Sprintf("%s=%s", key, value)
+		execCmd.Env = append(execCmd.Env, keyValue)
+	}
+	execCmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+
+	// when exec.CombinedOutput used and interrupt occurred, messages in stdout fails to get.
+	// in order to avoid above problem, we use new buffer.
+	var stdoutStderrBuffer bytes.Buffer
+	execCmd.Stdout = &stdoutStderrBuffer
+	execCmd.Stderr = &stdoutStderrBuffer
+
+	go func() {
+		err = execCmd.Run()
+	}()
+
+	// waiting for specified time
+	t := time.NewTimer(timeout)
+	<-t.C
+
+	// kill parent and child processes
+	if err := execCmd.Process.Kill(); err != nil {
+		return execCmd.ProcessState.ExitCode(), stdoutStderrBuffer.String(), err
+	}
+	err := syscall.Kill(-execCmd.Process.Pid, syscall.SIGKILL)
 	return execCmd.ProcessState.ExitCode(), stdoutStderrBuffer.String(), err
 }
